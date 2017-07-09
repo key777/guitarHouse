203723f845150c18f95134d0c0a422253d680700
key777 <499799736@qq.com>
2017-07-09 22:52:44 +0800

    yuheng
    首页优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,15 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    pageCfg: {
+      url: 'http://localhost/json/mainPage.json',
+      data: null
+    },
+    searchInfoHidden:false,
+    dailyList: [],
+    dailyTitle: null,
+    dailyConfig: null
   },
   //事件处理函数
   bindViewTap: function() {
@@ -13,14 +21,48 @@ Page({
     })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  
+    // 页面初始化 options为页面跳转所带来的参数
+    this.requestData('dailyList');
+  },
+
+  requestData: function (a) {
+    var that = this;
+      wx.request({
+      url: 'http://localhost:80/json/mainPage.json',
+      data: {
+        a: a,
+        c: 'data',
+        // 上一页的maxtime作为加载下一页的条件，
+        maxtime: this.data.maxtime,
+        type: '10',
+      },
+      method: 'GET',
+      success: function (res) {
+        console.log(res.data.dailyList)
+        console.log('上一页', that.data.dailyList)
+        that.setData({
+          // 拼接数组
+          dailyList: that.data.dailyList.concat(res.data.dailyList),
+          loadingHidden: true,
+          // maxtime: res.data.info.maxtime
+        })
+
+      }
+    })
+  },
+
+  /* 搜索框聚焦 */
+  searchBoxFocus: function(e){
+    this.setData({
+      searchInfoHidden: true
+    })
+  },
+
+  /* 搜索框失去焦点 */
+  searchBoxUrl: function(e){
+    this.setData({
+      searchInfoHidden: false
     })
   }
 })
